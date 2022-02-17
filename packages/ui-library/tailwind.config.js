8d6e6b0e4016b8c9361b2702b3e5cab06a0fc216
Igor <35524806+igorschoester@users.noreply.github.com>
2022-02-17 08:38:50 +0100

    Add tailwind JS support

--- packages/ui-library/tailwind.config.js
+++ packages/ui-library/tailwind.config.js
@@ -2,7 +2,7 @@
 const theme = require( "./src/theme" );
 
 module.exports = {
-	content: [ "./src/**/*.tsx" ],
+	content: [ "./src/**/*.{ts,tsx,js,jsx}" ],
 	theme: {
 		extend: {
 			colors: theme.colors
